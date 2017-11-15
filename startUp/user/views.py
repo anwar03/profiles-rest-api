493b46d6274ba3d36b2165966ec229dcf0d136ff
anwar03 <anwar@interconnectionbd.com>
2017-11-16 04:29:34 +0600

    Testing post, put, patch, delete request.

--- startUp/user/views.py
+++ startUp/user/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView feature."""
@@ -19,3 +22,38 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello world', 'an_apiview': an_apiview })
+
+
+    def post(self, request):
+        """Create a hello message with our name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            print('bang bang ', serializer.data.get('csrfmiddlewaretoken'))
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({ 'message': message })
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self, request, pk=None):
+        """Handles Updating an object."""
+
+        return Response({'message': 'put serializer is work.'})
+
+
+    def patch(self, request, pk=None):
+        """Patch request , only updates fields provides in the request."""
+
+        return Response({'message': 'patch serializer is work.'})
+
+
+    def delete(self, request, pk=None):
+        """Deletes and object."""
+
+        return Response({'message': 'delete serializer is work'})
