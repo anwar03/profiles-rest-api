2f2345fac10bba441df0f83bced080dab6304b66
anwar03 <anwar@interconnectionbd.com>
2017-11-19 01:48:24 +0600

    Added model for profile feed item and register to django admin.

--- startUp/user/urls.py
+++ startUp/user/urls.py
@@ -8,6 +8,7 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='bangBang-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     url(r'^hello-apiView/', views.HelloApiView.as_view()),
